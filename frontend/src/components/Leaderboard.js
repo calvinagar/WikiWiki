2e92b1e99fed79973cb52d1006457db6396d46d7
Kurt Postlmayr <kurtpostlmayr@Kurts-MacBook-Air.local>
2022-04-20 12:35:47 -0400

    added leaderboard component still just skeleton

---
+++ frontend/src/components/Leaderboard.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Table from 'react-bootstrap/Table'
+
+
+class Leaderboard extends Component {
+	constructor() { //add userid as parameter, so that we can populate data from constructor??
+        super();
+ 		this.state = {
+		  data: null,
+		}; 
+	}
+
+    //funct getdata
+        //using api pull number of people in leaderboards
+        //create blank array
+        //set each element of array equal to a person in the leaderboard and wrap for table element
+        //in render() below add the array wrapped in {}
+
+
+    render() {
+        return (
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                        <th>Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    //array object goes here
+                </tbody>
+            </Table>
+        );
+    }
+}
