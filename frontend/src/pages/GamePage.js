01ad4476b3a81e946e82d9fcaf7dfdd11f370ba1
Kurt Postlmayr <kurtpostlmayr@Kurts-MacBook-Air.local>
2022-04-30 11:51:29 -0400

    missing . from app.css imports

--- frontend/src/pages/GamePage.js
+++ frontend/src/pages/GamePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 import Sidebar from '../components/Sidebar'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
