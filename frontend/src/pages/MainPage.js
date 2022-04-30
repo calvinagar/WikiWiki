f02ef9b960558a7fc4f523be9999edfd9a3e4cbc
Kurt Postlmayr <kurtpostlmayr@Kurts-MacBook-Air.local>
2022-04-30 11:37:14 -0400

    added app.css to every file that needs it and removed from App.js

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -3,6 +3,7 @@ import Sidebar from '../components/Sidebar'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './App.css';
 import LongBriskCormorant from '../components/LongBriskCormorant.mp4';
 
 
