2efa318f63ab8f0a3fab6e4903ed1838bc97127f
Kurt Postlmayr <kurtpostlmayr@Kurts-MacBook-Air.local>
2022-04-30 15:38:49 -0400

    removed user info variable from leaderboard.js and changed the res.Leaderboard.... to res.leaderboard.... within the for loop declaration

--- frontend/src/components/Leaderboard.js
+++ frontend/src/components/Leaderboard.js
@@ -3,34 +3,28 @@ import Table from 'react-bootstrap/Table'
 
 
 export default class Leaderboard extends Component {
-	constructor() { //add userid as parameter, so that we can populate data from constructor??
+	constructor() {
         super();
  		this.state = {
-		  user: localStorage.getItem('user_data'),
           games: []
 		}; 
 
         this.getData();
 	}
 
-/*     componenetDidMount() {
-        fetch('/getPlayerGames')
-            .then(res => {console.log(res);})
-    } */
-
     getData = async () =>
     {
         try{
             var response = await fetch(buildPath('api/getDailyLeaderboard'), {method:'POST',headers:{'Content-Type': 'application/json'}});
             var res = JSON.parse(await response.text());
+            console.log(res);
 
             var tempGames = [];
 
-            for(let i = 0; i < res.Leaderboard.length; i++){
-                tempGames[i] = " <tr> <td>" + (i+1) + "</td>" +
+            for(let i = 1; i < res.leaderboard.length; i++){
+                tempGames[i] = " <tr> <td>" + i + "</td>" +
                                 " <td>" + res.leaderboard[i].login + "</td>" +
                                 " <td>" + res.leaderboard[i].clicks + "</td>" +
-                                " <td>" + res.leaderboard[i].time + "</td>" +
                                 " <td>" + res.leaderboard[i].startpage + "</td>" + 
                                 " <td>" + res.leaderboard[i].endpage + "</td> </tr>";
             }
@@ -43,10 +37,6 @@ export default class Leaderboard extends Component {
             alert(e.toString())
         }
     }
-        //using api pull number of people in leaderboards
-        //create blank array
-        //set each element of array equal to a person in the leaderboard and wrap for table element
-        //in render() below add the array wrapped in {}
 
 
     render() {
@@ -57,7 +47,6 @@ export default class Leaderboard extends Component {
                         <th>Rank</th>
                         <th>Username</th>
                         <th>Clicks</th>
-                        <th>Time</th>
                         <th>Start-Page</th>
                         <th>End-Page</th>
                     </tr>
