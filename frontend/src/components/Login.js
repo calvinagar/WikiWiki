36e1b72e2fb23f9b1f9e354b6bd0a32cdd778686
Skylar Marosi <33764428+smarosi1@users.noreply.github.com>
2022-05-01 14:39:38 -0400

    Added email to user_data
    On line 33, adding it to the user data which is cached in local storage. For later use with api.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -30,7 +30,7 @@ function Login()
             }
             else
             {
-                var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
+                var user = {firstName:res.firstName,lastName:res.lastName,id:res.id,email:res.email};
                 localStorage.setItem('user_data', JSON.stringify(user));
                 setMessage('');
                 window.location.href = '/main';
