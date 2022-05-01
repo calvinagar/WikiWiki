2a237a4423a0fff70e260c5fdd96a8e3b5b36e03
Aaron Crawford <aaronecrawford@gmail.com>
2022-05-01 18:55:31 -0400

    added Int parse for getLeaderboard

--- server.js
+++ server.js
@@ -651,10 +651,11 @@ app.post('/api/getDailyLeaderboard', async (req, res, next) =>
 {
   // incoming: 
   // outgoing: bool, daily leaderboard sorted by clicks
-  const numGames = req.body.numGames;
+  const numGames = parseInt(req.body.numGames);
 
   const db = client.db('largeProject');
   const sortLeaderboard = await
+
   db.collection('dailyLeaderboard').find({}).sort({clicks:1}).limit(numGames).toArray();
 
   
